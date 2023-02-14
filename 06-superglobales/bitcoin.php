be982b93a672a9668926c2b34c98e02c00648dd0
Matthieu Mota <matthieumota@gmail.com>
2023-02-14 14:54:22 +0100

    POST form

---
+++ 06-superglobales/bitcoin.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bitcoin vers euro</title>
+</head>
+<body>
+    <?php
+        $amount = $_POST['amount'] ?? null;
+        $currency = $_POST['currency'] ?? 'euro';
+
+        if (!empty($_POST)) { // On vérifie que le formulaire a été envoyé
+            $rate = 20295;
+
+            if ($currency === 'euro') { // Si la devise de départ est l'euro
+                $decimals = ($amount / $rate <= 1) ? 4 : 2; // 0,0004 ou 1 234,45
+                $result = number_format($amount / $rate, $decimals, ',', ' ');
+                echo "<h1>$amount euros valent $result bitcoins</h1>";
+            } else {
+                $result = number_format($amount * $rate, 2, ',', ' ');
+                echo "<h1>$amount bitcoins valent $result euros</h1>";
+            }
+        }
+    ?>
+
+    <form method="post" action="">
+        <div>
+            <label for="amount">Montant</label>
+            <input type="text" name="amount" id="amount" value="<?= $amount; ?>">
+        </div>
+
+        <div>
+            <label>Devise</label>
+            <input type="radio" name="currency" id="currency-euro" value="euro" <?= $currency === 'euro' ? 'checked' : ''; ?>>
+            <label for="currency-euro">Euros vers bitcoins</label>
+            <input type="radio" name="currency" id="currency-btc" value="btc" <?= $currency === 'btc' ? 'checked' : ''; ?>>
+            <label for="currency-btc">Bitcoins vers euros</label>
+        </div>
+
+        <button>Convertir</button>
+    </form>
+</body>
+</html>
